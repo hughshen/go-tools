398e5ae8a520d83efcf2bba24fd102f6b9a4b780
hughshen <hughssss@outlook.com>
2019-07-29 08:32:40 +0800

    Update fileserver

--- fileserver/fileserver.go
+++ fileserver/fileserver.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -19,7 +24,110 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
+	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/delete", deleteFile)
+	http.HandleFunc("/download", downloadFile)
 
 	log.Printf("Serving %s on HTTP %s\n", *directory, *host)
 	log.Fatal(http.ListenAndServe(*host, nil))
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		file, handler, err := r.FormFile("file")
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer file.Close()
+
+		absPath, _ := filepath.Abs("./" + handler.Filename)
+
+		if _, err = os.Stat(absPath); err == nil {
+			w.Write([]byte("File '" + handler.Filename + "' already exists"))
+			return
+		}
+
+		f, err := os.OpenFile(absPath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer f.Close()
+
+		io.Copy(f, file)
+
+		log.Printf("Upload file %s", handler.Filename)
+		w.Write([]byte("Upload success"))
+	} else {
+		w.Write([]byte("Invalid HTTP " + r.Method + " Method"))
+	}
+}
+
+func deleteFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		file := r.URL.Query().Get("file")
+		if len(file) < 1 {
+			w.Write([]byte("Url param 'file' is missing"))
+			return
+		}
+
+		absPath, _ := filepath.Abs("./" + file)
+
+		if _, err := os.Stat(absPath); os.IsNotExist(err) {
+			w.Write([]byte("File '" + file + "' doesn't exists"))
+			return
+		}
+
+		err := os.Remove(absPath)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		log.Printf("Delete file %s", file)
+		w.Write([]byte("Delete success"))
+	} else {
+		w.Write([]byte("Invalid HTTP " + r.Method + " Method"))
+	}
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		url := r.URL.Query().Get("url")
+		if len(url) < 1 {
+			w.Write([]byte("Url param 'url' is missing"))
+			return
+		}
+
+		_, file := path.Split(url)
+		fileName := strings.Split(file, "?")[0]
+		absPath, _ := filepath.Abs("./" + fileName)
+
+		if _, err := os.Stat(absPath); err == nil {
+			w.Write([]byte("File '" + fileName + "' already exists"))
+			return
+		}
+
+		fileOut, err := os.Create(absPath)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer fileOut.Close()
+
+		resp, err := http.Get(url)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer resp.Body.Close()
+
+		io.Copy(fileOut, resp.Body)
+
+		log.Printf("Download file %s", fileName)
+		w.Write([]byte("Download success"))
+	} else {
+		w.Write([]byte("Invalid HTTP " + r.Method + " Method"))
+	}
 }
